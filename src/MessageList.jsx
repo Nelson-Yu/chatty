625416d7fba8acbef0c907d10a71ebe99415ae5f
nelsonyu <nelson.tm.yu@gmail.com>
2019-06-04 18:46:14 +0000

    Added index key to remove warning

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -5,8 +5,8 @@ class MessageList extends Component {
   render() {
     return (
       <main className="messages">
-        {this.props.messages.map(message => 
-          <Message user={message.username} content={message.content}/>)}
+        {this.props.messages.map((message, index) => 
+          <Message key={index} user={message.username} content={message.content}/>)}
         <div className="message system">
           Anonymous1 changed their name to nomnom.
         </div>

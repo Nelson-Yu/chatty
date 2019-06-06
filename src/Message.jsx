7de7ff64fc1812285e354967f11cd4becc95a520
nelsonyu <nelson.tm.yu@gmail.com>
2019-06-06 23:19:26 +0000

    Added ability to send images via url links ending with img|png|gif

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,12 +1,27 @@
 import React, {Component, Fragment} from 'react';
 
 class Message extends Component {
+
+  const checkLink = (content) => {
+    const regExp = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&’\(\)\*\+,;=.]+(?:png|jpg|jpeg|gif|svg)+$/;
+    const pictureLink = content.match(regExp);
+    if(pictureLink) {
+      return pictureLink[0];
+    }
+    return false;
+  }
+
   render() {
+    const validLink = this.checkLink(this.props.content);
+
     return this.props.type === 'incomingMessage' ? (
+      <Fragment>
         <div className="message">
           <span className="message-username" style={{color:this.props.color}}>{this.props.user}</span>
-          <span className="message-content">{this.props.content}</span>
+          {!validLink && <span className="message-content">{this.props.content}</span>}
+          {validLink && <span className="message-picture"><img className="picture" src={validLink}/></span>}
         </div>
+      </Fragment>
     ) : (
         <div className="message system">
           {this.props.content}
